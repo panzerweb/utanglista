cc43c72a4353fe4f9ebbf859116b4e6b58451833
PanzerWeb <selwynvillar@gmail.com>
2025-03-10 15:55:31 +0800

    Prepared Starting Point
    - Added Index as Registration View
    - Added Templates for header and footer
    - Connected System to Database
    - Initiated Login view

---
+++ config/config.php
@@ -0,0 +1,19 @@
+<?php
+
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_table = "utanglista_db";
+
+try {
+    $connection = mysqli_connect($db_host, $db_user, $db_pass, $db_table);
+} catch (\Throwable $th) {
+    echo "Failed to Connect, error: " . $th;
+}
+
+if ($connection) {
+    echo "Connected Successfully";
+} else {
+    echo "Connection Failed!";
+}
+
--- index.php
+++ index.php
@@ -1,11 +1,42 @@
-<!DOCTYPE html>
+<!-- Registration and Homepage View -->
+<!-- Php Code for Registration -->
+<?php
+
+?>
+
+<!-- End of Php Code -->
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello</h1>
-</body>
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.3.3 -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    </head>
+
+    <body>
+        <!-- place navbar here -->
+        <?php 
+            //Header layout
+            include("./views/layout/header.php");
+        ?>
+        
+        <main>
+            <!-- Section for Registration -->
+             
+        </main>
+
+        <?php
+            include("./views/layout/footer.php")
+        ?>
+        
+        <!-- Bootstrap JavaScript Libraries -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </body>
 </html>
